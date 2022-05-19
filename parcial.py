f25a65d747c3bd91fd4b4675b6bac0f9771b638d
jpovalles <105830667+jpovalles@users.noreply.github.com>
2022-05-18 20:35:38 -0500

    Add files via upload

---
+++ parcial.py
@@ -0,0 +1,52 @@
+'''
+El siguiente programa tiene como finalidad simular la ejecución
+de la cafetería de la universidad, solicitando el número de cédula
+el rol dentro de la universidad, el código del producto que se
+desea comprar, la cantidad de unidades del producto, el precio unitario
+y preguntando si es beneficiario de una beca. Al final se imprime una cadena
+donde se mencionan el rol, la cédula, el código del product y el total de la
+compra sujeto a los respectivos descuentos.
+'''
+
+
+
+
+def cobroAlmuerzos():
+    while 1 > 0:    # Se pretende un funcionamiento continuo
+        print()
+        
+        ansBeca = False     # La beca no esta aplicada hasta que se ingrese lo contrario
+        cedula = int(input('Ingresa el número de tu cedula: '))
+        tipo = input('¿Eres profesor o estudiante? ')
+
+        while tipo != 'profesor' and tipo != 'estudiante':  # Validacion del rol
+            tipo = input('Ingresa un rol válido: ')
+
+            
+        if tipo == 'estudiante':
+            beca = input('¿Haces parte del programa de becas? ')
+
+            while beca != 'Si' and beca != 'No':
+                beca = input('¿Haces parte del programa de becas, Si o No? ')   # Validacion de una respuesta valida
+            
+            if beca == 'Si':
+                ansBeca = True
+                
+        producto = int(input('Ingresa el código del producto: '))
+        unidades = int(input('Ingresa la cantidad de unidades: '))
+        precio = int(input('Ingresa el precio unitario del producto: '))
+
+        subtotal = unidades * precio
+
+        # aplicacion del descuento
+        if tipo == 'profesor':
+            total = subtotal * 0.8
+        elif tipo == 'estudiante':
+            if ansBeca:
+                total = subtotal * 0.5
+            else:
+                total = subtotal * 0.7
+
+        print('El %s con cedula %d, debe pagar %d por el producto %d' %(tipo, cedula, total, producto))
+        print()
+        print('########################################################################')
